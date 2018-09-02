583153ba2513f9af5ccc558f6d3c86b0774c0faf
Lixing Song <lixingsong@hercules.csse.rose-hulman.edu>
2018-09-02 14:25:01 -0400

    trained model

--- pg-pong.py
+++ pg-pong.py
@@ -18,6 +18,7 @@ render = True
 D = 80 * 80 # input dimensionality: 80x80 grid
 if resume:
   model = pickle.load(open('save.p', 'rb'))
+  model = pickle.load(open('save-4600.p', 'rb'))
 else:
   model = {}
   model['W1'] = np.random.randn(H,D) / np.sqrt(D) # "Xavier" initialization
